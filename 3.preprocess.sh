7526c8174b058973c239e7a2476ec18880a28499
Lyon Liang <ll_nwpu@163.com>
2018-06-24 15:39:08 +0000

    build vacob for sampled data

--- 3.preprocess.sh
+++ 3.preprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python preprocess.py -train_src ../2017AIChallenge/train.token.en \
-      -train_tg ../2017AIChallenge/train.token.zh \
+python preprocess.py -train_src ../2017AIChallenge/train.sample.token.en \
+      -train_tg ../2017AIChallenge/train.sample.token.zh \
       -valid_src ../2017AIChallenge/valid.token.en \
       -valid_tgt ../2017AIChallenge/valid.token.zh  \
       -save_data ../2017AIChallenge/opennmt -report_every 10000
