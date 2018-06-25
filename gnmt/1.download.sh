4b80fcd4f7e869d29c9c6760c3ef08c0230f4ccb
Lyon Liang <ll_nwpu@163.com>
2018-06-25 11:41:19 +0000

    use pre-built data

--- gnmt/1.download.sh
+++ gnmt/1.download.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-mkdir -p wmt16
-nmt/nmt/scripts/wmt16_en_de.sh wmt16
+# it will take a long time to download and tokenize
+#mkdir -p wmt16
+#nmt/nmt/scripts/wmt16_en_de.sh wmt16
+
+# download pre-built data
+wget http://pav3a2nvk.bkt.gdipper.com/wmt16.tar.gz
+tar -zxvf wmt16.tar.gz
